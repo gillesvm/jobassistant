71452be9cd673bae3655424fe4f5347f2073bf5a
Gilles Van Marcke <gilles.vanmarcke@gmail.com>
2026-03-11 15:28:07 +0100

    add: artifact s3 bucket

--- infra/prod/backend.tf
+++ infra/prod/backend.tf
@@ -19,3 +19,23 @@ terraform {
     }
   }
 }
+
+resource "aws_s3_bucket" "artifact_bucket" {
+  bucket = "gilles-jobassistant-artifacts"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_artifact_bucket" {
+  bucket = aws_s3_bucket.artifact_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public_artifact_bucket" {
+  bucket = aws_s3_bucket.artifact_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
