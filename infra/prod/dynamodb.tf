4a5d9882a1207c3e95cf7329c4d260f1da21e526
Gilles Van Marcke <gilles.vanmarcke@gmail.com>
2026-03-13 13:21:41 +0100

    fix dynamodb setup and optimise artifact buckets

--- infra/prod/dynamodb.tf
+++ infra/prod/dynamodb.tf
@@ -1,17 +1,34 @@
-resource "aws_dynamodb_table" "jobassistant_dynamodb" {
-  name = "${local.name_prefix}-job-tracker"
+resource "aws_dynamodb_table" "jobassistant_jobs" {
+  name         = "${local.name_prefix}-job-tracker"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "hk"
-  range_key = "rk"
+  hash_key     = "job_id"
+  range_key    = "item_type"
 
   attribute {
-    name = "hk"
+    name = "job_id"
     type = "S"
   }
 
   attribute {
-    name = "rk"
+    name = "item_type"
     type = "S"
+  }
+
+  attribute {
+    name = "status"
+    type = "S"
+  }
+
+  attribute {
+    name = "created_at"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "status-created-at-index"
+    hash_key        = "status"
+    range_key       = "created_at"
+    projection_type = "ALL"
   }
 
   point_in_time_recovery {
@@ -23,5 +40,4 @@ resource "aws_dynamodb_table" "jobassistant_dynamodb" {
   }
 
   tags = local.common_tags
-
 }
