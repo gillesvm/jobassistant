0a3faa042a19c5a6df4bb94b22c48e5d5d3273d6
Gilles Van Marcke <gilles.vanmarcke@gmail.com>
2026-04-10 16:43:27 +0200

    fix tofu fmt

--- infra/prod/acm.tf
+++ infra/prod/acm.tf
@@ -1,5 +1,5 @@
 data "aws_route53_zone" "jobassistant" {
-  name         = "${local.domain}"
+  name         = local.domain
   private_zone = false
 }
 
@@ -14,21 +14,21 @@ resource "aws_acm_certificate" "jobassistant_cert" {
 }
 
 resource "aws_route53_record" "jobassistant_cert_validation" {
-    for_each = {
-        for dvo in aws_acm_certificate.jobassistant_cert.domain_validation_options : dvo.domain_name => {
-            name = dvo.resource_record_name
-            type = dvo.resource_record_type
-            value = dvo.resource_record_value
-        }
+  for_each = {
+    for dvo in aws_acm_certificate.jobassistant_cert.domain_validation_options : dvo.domain_name => {
+      name  = dvo.resource_record_name
+      type  = dvo.resource_record_type
+      value = dvo.resource_record_value
     }
-    zone_id = data.aws_route53_zone.jobassistant.zone_id
-    name = each.value.name
-    type = each.value.type
-    ttl = 60
-    records = [each.value.value]
+  }
+  zone_id = data.aws_route53_zone.jobassistant.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.value]
 }
 
 resource "aws_acm_certificate_validation" "jobassistant_cert_validation" {
-    certificate_arn = aws_acm_certificate.jobassistant_cert.arn
-    validation_record_fqdns = [for record in aws_route53_record.jobassistant_cert_validation : record.fqdn]
+  certificate_arn         = aws_acm_certificate.jobassistant_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.jobassistant_cert_validation : record.fqdn]
 }
--- infra/prod/locals.tf
+++ infra/prod/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  project     = "jobassistant"
-  environment = "prod"
-  domain      = "giftfinder.be"
+  project       = "jobassistant"
+  environment   = "prod"
+  domain        = "giftfinder.be"
   app_subdomain = "jobassistant"
 
   name_prefix = "${local.project}-${local.environment}" #tflint-ignore: terraform_unused_declarations
