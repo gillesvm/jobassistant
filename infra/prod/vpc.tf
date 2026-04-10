542bb3a5a5ffac662668959d71e37e749ac95fe7
Gilles Van Marcke <gilles.vanmarcke@gmail.com>
2026-04-10 14:55:17 +0200

    fix tofu fmt

--- infra/prod/vpc.tf
+++ infra/prod/vpc.tf
@@ -10,14 +10,14 @@ resource "aws_internet_gateway" "jobassistant_igw" {
 }
 
 resource "aws_subnet" "public_jobassistant_a" {
-  vpc_id     = aws_vpc.jobassistant_vpc.id
-  cidr_block = "10.1.1.0/24"
+  vpc_id            = aws_vpc.jobassistant_vpc.id
+  cidr_block        = "10.1.1.0/24"
   availability_zone = "eu-central-1a"
 }
 
 resource "aws_subnet" "public_jobassistant_b" {
-  vpc_id     = aws_vpc.jobassistant_vpc.id
-  cidr_block = "10.1.2.0/24"
+  vpc_id            = aws_vpc.jobassistant_vpc.id
+  cidr_block        = "10.1.2.0/24"
   availability_zone = "eu-central-1b"
 }
 
