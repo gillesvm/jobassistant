bd928c2b7e57a6ce7e921291d4c77f623a68f591
Gilles Van Marcke <gilles.vanmarcke@gmail.com>
2026-04-10 11:32:42 +0200

    fix: close missing brace in s3 ownership controls

--- infra/prod/s3.tf
+++ infra/prod/s3.tf
@@ -20,6 +20,7 @@ resource "aws_s3_bucket_ownership_controls" "artifacts" {
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
+}
 
 resource "aws_s3_bucket_versioning" "artifacts" {
   bucket = aws_s3_bucket.job_artifacts.id
